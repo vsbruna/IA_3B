ced514980298cf0e56ea8da0b1c5d0053505bbc4
vsbruna <141277861+vsbruna@users.noreply.github.com>
2025-05-23 11:25:14 -0300

    okokokokokoklalalalalalalala

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textResultado = document.querySelector(".texto=resultado");
